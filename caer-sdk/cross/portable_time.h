c67b4c5f53f9c99890d968cdd31be6d82c68f727
Luca Longinotti <l@longi.li>
2018-12-21 19:53:50 +0100

    Portable Time: add new function portable_clock_localtime().

--- caer-sdk/cross/portable_time.h
+++ caer-sdk/cross/portable_time.h
@@ -3,14 +3,14 @@
 
 #ifdef __cplusplus
 
-#include <cstdlib>
-#include <ctime>
+#	include <cstdlib>
+#	include <ctime>
 
 #else
 
-#include <stdbool.h>
-#include <stdlib.h>
-#include <time.h>
+#	include <stdbool.h>
+#	include <stdlib.h>
+#	include <time.h>
 
 #endif
 
@@ -20,6 +20,7 @@ extern "C" {
 
 bool portable_clock_gettime_monotonic(struct timespec *monoTime);
 bool portable_clock_gettime_realtime(struct timespec *realTime);
+struct tm portable_clock_localtime(void);
 
 #ifdef __cplusplus
 }
