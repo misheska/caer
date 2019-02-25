334696f3c0f0e9993b26ec787a5b00136d53d6b1
Luca Longinotti <luca.longinotti@inivation.com>
2019-02-25 17:47:34 +0100

    flatbuffers: use own dv::cvector vector type, can be made C and multi-compiler compatible.

--- dv-sdk/events/dv-sdk-flatc.sh
+++ dv-sdk/events/dv-sdk-flatc.sh
@@ -1,3 +1,3 @@
-flatc --cpp --scoped-enums --gen-object-api --reflect-types --reflect-names "${1}".fbs
+flatc --cpp --scoped-enums --gen-object-api --cpp-vec-type "dv::cvector" --reflect-types --reflect-names "${1}".fbs
 mv "${1}"_generated.h "${1}".h
 clang-format -i "${1}".h
