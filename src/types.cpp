3cc962ef03ae8fc7554752e40b4fa27b664a0d15
Luca Longinotti <luca.longinotti@inivation.com>
2019-03-11 11:04:18 +0100

    Types: format, implement lookup, add locking.

--- src/types.cpp
+++ src/types.cpp
@@ -9,61 +9,91 @@
 namespace dv::types {
 
 TypeSystem::TypeSystem() {
-    // Initialize system types. These are always available due to
-    // being compiled into the core.
-    systemTypes.emplace_back("SPEC", nullptr, nullptr);
+	// Initialize system types. These are always available due to
+	// being compiled into the core.
+	systemTypes.emplace_back("SPEC", nullptr, nullptr);
 
-    systemTypes.emplace_back(PolarityPacketIdentifier(), reinterpret_cast<PackFuncPtr>(&PolarityPacket::Pack),
-        reinterpret_cast<UnpackFuncPtr>(&PolarityPacket::UnPackToFrom));
+	systemTypes.emplace_back(PolarityPacketIdentifier(), reinterpret_cast<PackFuncPtr>(&PolarityPacket::Pack),
+		reinterpret_cast<UnpackFuncPtr>(&PolarityPacket::UnPackToFrom));
 
-    systemTypes.emplace_back(Frame8PacketIdentifier(), reinterpret_cast<PackFuncPtr>(&Frame8Packet::Pack),
-        reinterpret_cast<UnpackFuncPtr>(&Frame8Packet::UnPackToFrom));
+	systemTypes.emplace_back(Frame8PacketIdentifier(), reinterpret_cast<PackFuncPtr>(&Frame8Packet::Pack),
+		reinterpret_cast<UnpackFuncPtr>(&Frame8Packet::UnPackToFrom));
 
-    systemTypes.emplace_back("IMU9", nullptr, nullptr);
+	systemTypes.emplace_back("IMU9", nullptr, nullptr);
 }
 
 void TypeSystem::registerType(const Type t) {
-    // Register user type. Rules:
-    // a) cannot have same identifier as a system type
-    // b) if same type registered multiple times, just
-    // add multiple times. Modules will register on
-    // load/dlopen, and unregister on unload/dlclose.
-    // As such, a pack/unpack will always be present if
-    // any module that uses such a type is loaded at least
-    // once. It is assumed types with same identifier are
-    // equal or fully compatible.
-    if (findIfBool(
-            systemTypes.cbegin(), systemTypes.cend(), [&t](const Type &sysType) { return (t.id == sysType.id); })) {
-        boost::format msg = boost::format("Failed to register type %s, already a system type.") % t.identifier;
+	std::lock_guard<std::recursive_mutex> lock(typesLock);
 
-        caerLog(CAER_LOG_ERROR, "Types", msg.str().c_str());
-        throw std::invalid_argument(msg.str().c_str());
-    }
+	// Register user type. Rules:
+	// a) cannot have same identifier as a system type
+	// b) if same type registered multiple times, just
+	// add multiple times. Modules will register on
+	// load/dlopen, and unregister on unload/dlclose.
+	// As such, a pack/unpack will always be present if
+	// any module that uses such a type is loaded at least
+	// once. It is assumed types with same identifier are
+	// equal or fully compatible.
+	if (findIfBool(
+			systemTypes.cbegin(), systemTypes.cend(), [&t](const Type &sysType) { return (t.id == sysType.id); })) {
+		boost::format msg = boost::format("Failed to register type %s, already a system type.") % t.identifier;
 
-    // Not a system type. Add to list.
-    userTypes[t.id].push_back(t);
+		caerLog(CAER_LOG_ERROR, "Types", msg.str().c_str());
+		throw std::invalid_argument(msg.str().c_str());
+	}
+
+	// Not a system type. Add to list.
+	userTypes[t.id].push_back(t);
 }
 
 void TypeSystem::unregisterType(const Type t) {
-    if (userTypes.count(t.id) == 0) {
-        // Non existing type, error!
-        boost::format msg = boost::format("Failed to unregister type %s, type does not exist.") % t.identifier;
+	std::lock_guard<std::recursive_mutex> lock(typesLock);
 
-        caerLog(CAER_LOG_ERROR, "Types", msg.str().c_str());
-        throw std::invalid_argument(msg.str().c_str());
-    }
+	if (userTypes.count(t.id) == 0) {
+		// Non existing type, error!
+		boost::format msg = boost::format("Failed to unregister type %s, type does not exist.") % t.identifier;
 
-    auto &vec = userTypes[t.id];
-    auto pos  = std::find(vec.cbegin(), vec.cend(), t);
+		caerLog(CAER_LOG_ERROR, "Types", msg.str().c_str());
+		throw std::invalid_argument(msg.str().c_str());
+	}
 
-    if (pos != vec.cend()) {
-        vec.erase(pos);
-    }
+	auto &vec = userTypes[t.id];
+	auto pos  = std::find(vec.cbegin(), vec.cend(), t);
 
-    // Remove empty types.
-    if (vec.empty()) {
-        userTypes.erase(t.id);
-    }
+	if (pos != vec.cend()) {
+		vec.erase(pos);
+	}
+
+	// Remove empty types.
+	if (vec.empty()) {
+		userTypes.erase(t.id);
+	}
+}
+
+Type TypeSystem::getTypeInfo(const char *tIdentifier) {
+	uint32_t id = *(reinterpret_cast<const uint32_t *>(tIdentifier));
+
+	return (getTypeInfo(id));
+}
+
+Type TypeSystem::getTypeInfo(uint32_t tId) {
+	std::lock_guard<std::recursive_mutex> lock(typesLock);
+
+	// Search for type, first in system then user types.
+	auto sysPos
+		= std::find_if(systemTypes.cbegin(), systemTypes.cend(), [tId](const Type &t) { return (t.id == tId); });
+
+	if (sysPos != systemTypes.cend()) {
+		// Found.
+		return (*sysPos);
+	}
+
+	if (userTypes.count(tId) > 0) {
+		return (userTypes[tId].at(0));
+	}
+
+	// Not fund.
+	throw std::out_of_range("Type not found in type system.");
 }
 
 } // namespace dv::types
--- src/types.hpp
+++ src/types.hpp
@@ -7,40 +7,45 @@
 
 #include <array>
 #include <map>
+#include <mutex>
 
 namespace dv::types {
 
 using PackFuncPtr = flatbuffers::Offset<void> (*)(
-    flatbuffers::FlatBufferBuilder &, const void *, const flatbuffers::rehasher_function_t *);
+	flatbuffers::FlatBufferBuilder &, const void *, const flatbuffers::rehasher_function_t *);
 using UnpackFuncPtr = void (*)(void *, const void *, const flatbuffers::resolver_function_t *);
 
 struct Type {
-    uint32_t id;
-    char identifier[5];
-    PackFuncPtr pack;
-    UnpackFuncPtr unpack;
+	uint32_t id;
+	char identifier[5];
+	PackFuncPtr pack;
+	UnpackFuncPtr unpack;
 
-    Type(const char *_identifier, PackFuncPtr _pack, UnpackFuncPtr _unpack) : pack(_pack), unpack(_unpack) {
-        // Four character identifier, NULL terminated.
-        memcpy(identifier, _identifier, 4);
-        identifier[4] = '\0';
+	Type(const char *_identifier, PackFuncPtr _pack, UnpackFuncPtr _unpack) : pack(_pack), unpack(_unpack) {
+		// Four character identifier, NULL terminated.
+		memcpy(identifier, _identifier, 4);
+		identifier[4] = '\0';
 
-        // Transform into 32bit integer for fast comparison.
-        // Does not have to be endian-neutral, only used internally.
-        id = *(reinterpret_cast<uint32_t *>(identifier));
-    }
+		// Transform into 32bit integer for fast comparison.
+		// Does not have to be endian-neutral, only used internally.
+		id = *(reinterpret_cast<uint32_t *>(identifier));
+	}
 };
 
 class TypeSystem {
 private:
-    std::vector<Type> systemTypes;
-    std::map<uint32_t, std::vector<Type>> userTypes;
+	std::vector<Type> systemTypes;
+	std::map<uint32_t, std::vector<Type>> userTypes;
+	std::recursive_mutex typesLock;
 
 public:
-    TypeSystem();
+	TypeSystem();
 
-    void registerType(const Type t);
-    void unregisterType(const Type t);
+	void registerType(const Type t);
+	void unregisterType(const Type t);
+
+	Type getTypeInfo(const char *tIdentifier);
+	Type getTypeInfo(uint32_t tId);
 };
 
 } // namespace dv::types
