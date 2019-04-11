05ddd1e8fc13fc11b7c260ff54ba1a9ecde505c2
Luca Longinotti <luca.longinotti@inivation.com>
2019-04-11 13:41:46 +0200

    SDK flatbuffers: move flatc into flatbuffers directory.

--- dv-sdk/data/dv-sdk-flatc.sh
+++ dv-sdk/data/dv-sdk-flatc.sh
@@ -1,3 +1,3 @@
-./flatc --cpp --scoped-enums --gen-object-api --gen-compare --gen-name-strings --cpp-ptr-type "std::unique_ptr" --cpp-str-type "dv::cstring" --cpp-str-flex-ctor --cpp-vec-type "dv::cvector" --reflect-types --reflect-names "${1}".fbs
+./flatbuffers/flatc --cpp --scoped-enums --gen-object-api --gen-compare --gen-name-strings --cpp-ptr-type "std::unique_ptr" --cpp-str-type "dv::cstring" --cpp-str-flex-ctor --cpp-vec-type "dv::cvector" --reflect-types --reflect-names "${1}".fbs
 mv "${1}"_generated.h "${1}".hpp
 clang-format -i "${1}".hpp
